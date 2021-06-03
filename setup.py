de923c95612d22091a39510deb38db9f559e3c56
Pascal Audet <pascal.audet@uottawa.ca>
2021-06-03 13:00:00 -0400

    added python=3.9 support

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
          'Programming Language :: Python :: 3.7',
          'Programming Language :: Python :: 3.8',
          'Programming Language :: Python :: 3.9'],
-    install_requires=['numpy', 'obspy', 'stdb>=0.2.0', 'cartopy'],
+    install_requires=['numpy', 'obspy', 'stdb>=0.2.0'],
     python_requires='>=3.6',
     packages=['rfpy'],
     entry_points={'console_scripts':[
